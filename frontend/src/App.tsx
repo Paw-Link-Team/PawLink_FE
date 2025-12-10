12f58593cdfb1e867bc60cd65950eedfe71de10c
woohyuk0428 <whkang03@gmail.com>
2025-12-10 16:52:10 +0900

    feat/#9: 외부접속 허용 및 port 번호 3000으로 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import WalkRank from "./components/WalkRank/WalkRank";
 import Home from "./Home";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="app-root">
       {/* <Home /> */}
       <Header location="서울시 강남구" />
+      <WalkRank />
       <Navbar />
     </div>
   );
